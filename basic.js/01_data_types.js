eae29df20e00a01f5dd70dfaa05942a92ee9b2da
ahaan955 <ahaanraikhare@gmail.com>
2025-06-12 15:01:36 +0000

    learned data base &  variables

---
+++ basic.js/01_data_types.js
@@ -0,0 +1,15 @@
+"use strict"// for new js codes
+
+// alert ( 3+3 )// we are using git not browser
+
+console.log( 3+3 );
+// types of data 
+// number =======> 2 to the power od 53 
+// string=====> words 
+// bights 
+// boolean =======> true & fals 
+// null
+// undefined =======> no assined value 
+// symbols 
+// object
+console.log(typeof 2 );
---
+++ basic.js/1_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12112
+let accountEmail = "ahaan@gmail.com"
+var accountPassword = "ahaan@123"
+accountCity = "kanpur"
+
+// accountId = 2
+
+accountEmail = "apajaj"
+accountPassword = "1221212"
+accountCity = "ahaan"
+/*
+dont use var
+because of issues 
+*/
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
